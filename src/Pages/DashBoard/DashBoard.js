4931be578655c082e750faacf0991eaff74924da
araf78 <alarafath71@gmail.com>
2022-05-29 22:23:52 +0600

    admin

--- src/Pages/DashBoard/DashBoard.js
+++ src/Pages/DashBoard/DashBoard.js
@@ -20,7 +20,12 @@ const Dashboard = () => {
                 <ul className="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
                  {
-                    
+                     !admin && <>
+                        <li><Link to="/dashboard">My Orders</Link></li>
+                        <li><Link to="/dashboard/addreview">Add Reviews</Link></li>
+                        <li><Link to="/dashboard/myprofile">My Profile</Link></li>
+                        <li><Link to="/dashboard/users">All Users</Link></li>
+                     </>
                  }
                     { admin && <>
                         <li><Link to="/dashboard/myprofile">My Profile</Link></li>
