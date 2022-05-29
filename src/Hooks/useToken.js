5e52c642154581c7dfa6afe730994a6cdab2d736
araf78 <alarafath71@gmail.com>
2022-05-29 22:27:24 +0600

    remove fetch token

--- src/Hooks/useToken.js
+++ src/Hooks/useToken.js
@@ -9,13 +9,7 @@ const useToken = (user) =>{
     useEffect(()=>{
         // console.log('user inside useToken', user);
         if(email){
-            fetch(`http://localhost:5000/user/${email}`, {
-                method:'PUT',
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body:JSON.stringify(currentUser)
-            })
+           
             .then(res=>res.json())
             .then(data => {
                 console.log('data inside useToken', data);
